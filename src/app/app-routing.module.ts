75dc135c4bc593bece56a5cfc7a99543be081dbc
Hakony <anphamhkn@gmail.com>
2023-06-16 11:34:45 -0700

    update dev and prod href

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { DocumentationComponent } from './documentation/documentation.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -13,6 +14,10 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  // providers: [
+  //   { provide: LocationStrategy, useClass: HashLocationStrategy }
+  // ]
 })
+
 export class AppRoutingModule { }
